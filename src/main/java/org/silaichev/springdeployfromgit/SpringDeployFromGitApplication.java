620e131071e62d17ff96506821655fcc0f4359f6
silai <silaichev1999@gmail.com>
2024-03-20 10:50:53 +0200

    Initial commit

---
+++ src/main/java/org/silaichev/springdeployfromgit/SpringDeployFromGitApplication.java
@@ -0,0 +1,13 @@
+package org.silaichev.springdeployfromgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployFromGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeployFromGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/silaichev/springdeployfromgit/SpringDeployFromGitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.silaichev.springdeployfromgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployFromGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
