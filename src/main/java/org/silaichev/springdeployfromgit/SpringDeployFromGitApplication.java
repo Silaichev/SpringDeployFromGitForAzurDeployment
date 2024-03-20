150e1f16e4f8bc9e46db527da992e7b2477a7c27
silai <silaichev1999@gmail.com>
2024-03-20 10:56:37 +0200

    add filling

--- src/main/java/org/silaichev/springdeployfromgit/SpringDeployFromGitApplication.java
+++ src/main/java/org/silaichev/springdeployfromgit/SpringDeployFromGitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDeployFromGitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDeployFromGitApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeployFromGitApplication.class, args);
+    }
 
 }
--- src/main/java/org/silaichev/springdeployfromgit/controller/DefaultController.java
+++ src/main/java/org/silaichev/springdeployfromgit/controller/DefaultController.java
@@ -1,4 +1,14 @@
+package org.silaichev.springdeployfromgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
+
+    @GetMapping("/get")
+    public String defaultGet(){
+
+        return "defGetPage";
+    }
 }
